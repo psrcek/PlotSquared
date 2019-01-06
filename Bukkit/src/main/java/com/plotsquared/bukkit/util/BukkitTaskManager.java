85cdcd1940d547d3bc7eab4b3b45cab72798a816
David Panić <david@panic.tk>
2019-01-06 03:56:20 +0100

    C a n c e r

--- Bukkit/src/main/java/com/plotsquared/bukkit/util/BukkitTaskManager.java
+++ Bukkit/src/main/java/com/plotsquared/bukkit/util/BukkitTaskManager.java
@@ -8,12 +8,12 @@ import org.bukkit.scheduler.BukkitScheduler;
 import java.util.List;
 import java.util.ArrayList;
 import java.util.Queue;
-import java.util.concurrent.PriorityBlockingQueue;
+import java.util.concurrent.ArrayBlockingQueue;
 
 public class BukkitTaskManager extends TaskManager {
     private final BukkitMain bukkitMain;
 
-    private final Queue<Runnable> taskQueue = new PriorityBlockingQueue<>();
+    private final Queue<Runnable> taskQueue = new ArrayBlockingQueue<>(100000);
     private final List<Integer> ids = new ArrayList<>();
 
     public BukkitTaskManager(final BukkitMain bukkitMain) {
@@ -28,18 +28,24 @@ public class BukkitTaskManager extends TaskManager {
                         int id = ids.get(i);
 
                         if (!sched.isCurrentlyRunning(id) && !sched.isQueued(id)) {
+                            System.out.println("[FagSquared] Task finished: " + id + ", remaining tasks: " + ids.size());
                             ids.remove(i);
                         }
                     }
 
                     if (ids.size() > 2000) {
+                        System.out.println("[FagSquared] More than 2000 tasks are running, skipping adding a new task.");
                         return;
                     }
 
                     Runnable tsk = taskQueue.poll();
 
                     if (tsk != null) {
+                        System.out.println("[FagSquared] Running new task: " + tsk);
+
                         int id = bukkitMain.getServer().getScheduler().runTaskAsynchronously(bukkitMain, tsk).getTaskId();
+
+                        System.out.println("[FagSquared] Started task: " + id);
 
                         ids.add(id);
                     }
@@ -62,6 +68,8 @@ public class BukkitTaskManager extends TaskManager {
     public void taskAsync(Runnable runnable) {
         //this.bukkitMain.getServer().getScheduler().runTaskAsynchronously(this.bukkitMain, runnable).getTaskId();
         taskQueue.add(runnable);
+
+        System.out.println("[FagSquared] New runnable added to run async in future: " + runnable);
     }
 
     @Override
