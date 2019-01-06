9e0c2120bd9be79e145e62f6d88d35dd28de1bc8
David Panić <david@panic.tk>
2019-01-06 03:02:02 +0100

    Fixed thread stuff crash

--- Bukkit/src/main/java/com/plotsquared/bukkit/util/BukkitTaskManager.java
+++ Bukkit/src/main/java/com/plotsquared/bukkit/util/BukkitTaskManager.java
@@ -3,13 +3,45 @@ package com.plotsquared.bukkit.util;
 import com.intellectualcrafters.plot.util.TaskManager;
 import com.plotsquared.bukkit.BukkitMain;
 import org.bukkit.Bukkit;
+import org.bukkit.scheduler.BukkitScheduler;
+
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Queue;
+import java.util.concurrent.PriorityBlockingQueue;
 
 public class BukkitTaskManager extends TaskManager {
-
     private final BukkitMain bukkitMain;
+
+    private synchronized final Queue<Runnable> taskQueue = new Queue<>();
+    private synchronized final List<int> ids = new ArrayList<>();
 
     public BukkitTaskManager(BukkitMain bukkitMain) {
         this.bukkitMain = bukkitMain;
+
+            this.bukkitMain.getServer().getScheduler().scheduleAsyncRepeatingTask(this.bukkitMain, () -> {
+                BukkitScheduler sched = this.bukkitMain.getServer().getScheduler();
+
+                for (int i = 0; i < ids.size()) {
+                    int id = ids.get(i);
+
+                    if (!sched.isCurrentlyRunning(id) && !sched.isQueued(id)) {
+                        ids.remove(i);
+                    }
+                }
+
+                if (ids.size() > 2000) {
+                    return;
+                }
+
+                Runnable tsk = taskQueue.poll();
+
+                if (tsk != null) {
+                    int id = this.bukkitMain.getServer().getScheduler().runTaskAsynchronously(this.bukkitMain, runnable).getTaskId();
+
+                    ids.add(id);
+                }
+            }, 1, 1);
     }
 
     @Override
@@ -25,7 +57,8 @@ public class BukkitTaskManager extends TaskManager {
 
     @Override
     public void taskAsync(Runnable runnable) {
-        this.bukkitMain.getServer().getScheduler().runTaskAsynchronously(this.bukkitMain, runnable).getTaskId();
+        //this.bukkitMain.getServer().getScheduler().runTaskAsynchronously(this.bukkitMain, runnable).getTaskId();
+        taskQueue.add(runnable);
     }
 
     @Override
